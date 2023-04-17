6bb0fa41d0b9ca41cd1efb31d99b9e3379b8a1f7
pcbennett108 <pcbennett42@gmail.com>
2023-04-17 11:13:50 -0600

    Complete creature dragon

---
+++ ruby_exercises/mythical-creatures/lib/dragon.rb
@@ -0,0 +1,26 @@
+class Dragon
+    attr_reader :name, :rider, :color, :hungry
+    def initialize(name, color, rider)
+        @name = name
+        @color = color
+        @rider = rider
+        @hungry = true
+        @meals = 0
+    end
+
+    def hungry?
+        @hungry
+    end
+
+    def eat
+        @meals += 1
+        if @meals >= 3
+            @hungry = false
+        end
+    end
+end
+
+# dragon = Dragon.new('Mnementh', :bronze, 'Flar')
+# p dragon.hungry?
+# dragon.eat
+# p dragon.hungry?
