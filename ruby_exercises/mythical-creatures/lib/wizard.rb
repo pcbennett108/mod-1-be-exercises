44d3f69c16174bbcbb83faba0abf5c412059fad1
pcbennett108 <pcbennett42@gmail.com>
2023-04-18 13:58:23 -0600

    Complete tests for wizard.rb

---
+++ ruby_exercises/mythical-creatures/lib/wizard.rb
@@ -0,0 +1,43 @@
+class Wizard
+    attr_reader :name, :bearded, :incantation, :rested, :spells_cast
+    def initialize(name, bearded: true)
+        @name = name
+        @bearded = bearded
+        @rested = true
+        @spells_cast = 0
+    end
+
+    def bearded?
+        @bearded
+    end
+
+    def incantation(incant)
+        "sudo #{incant}"
+    end
+
+    def rested?
+        @rested
+    end
+
+    def cast
+        @spells_cast += 1
+        if @spells_cast < 3
+            "MAGIC MISSILE!"
+        elsif @spells_cast = 3
+            @rested = false
+            "MAGIC MISSILE!"
+        else
+            "Too tired to cast,"
+        end
+    end
+end
+
+# wizard2 = Wizard.new('Sal', bearded: true)
+# p wizard2
+# #p wizard2.bearded?
+# p wizard2.rested?
+# p wizard2.cast
+# p wizard2.cast
+# p wizard2.rested?
+# p wizard2.cast
+# p wizard2.rested?
