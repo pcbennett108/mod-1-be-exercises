4f745b4937b49fc62b91d4f8c9752642d36fbe3e
pcbennett108 <pcbennett42@gmail.com>
2023-04-17 19:22:23 -0600

    Complete tests 8 - END for hobbit.rb

--- ruby_exercises/mythical-creatures/lib/hobbit.rb
+++ ruby_exercises/mythical-creatures/lib/hobbit.rb
@@ -1,10 +1,13 @@
 class Hobbit
-    attr_reader :name, :disposition, :age, :adult
+    attr_reader :name, :disposition, :age, :adult, :old, :has_ring, :is_short
     def initialize(name, disposition = 'homebody')
         @name = name
         @disposition = disposition
         @age = 0
         @adult = false
+        @old = false
+        @has_ring = false
+        @is_short = true
     end
 
     def celebrate_birthday
@@ -18,12 +21,21 @@ class Hobbit
         @adult
     end
 
+    def old?
+        if @age >= 101
+            @old = true
+        end
+        @old
+    end
+
+    def has_ring? # Only changes has_ring if checked, not on creation.
+        if @name == "Frodo"
+            @has_ring = true
+        end
+        @has_ring
+    end
+
+    def is_short?
+        @is_short
+    end
 end
-
-
-
-# hobbit1 = Hobbit.new("Samwise", "Loyal")
-# p hobbit1.age
-# p hobbit1.name
-# p hobbit1.adult
-# p hobbit1
