44cf958b35b17d0318ce85c8a693a56a7babe244
pcbennett108 <pcbennett42@gmail.com>
2023-04-28 20:44:48 -0600

    Complete Narwhal and Octopus initialize exercises

---
+++ ruby_exercises/initialize/lib/narwhal.rb
@@ -0,0 +1,11 @@
+class Narwhal
+    attr_reader :name, :weight, :cute
+    def initialize(attributes)
+        @name = attributes[:name]
+        @weight = attributes[:weight]
+        @cute = attributes[:cute]
+    end
+    def cute?
+        cute
+    end
+end
